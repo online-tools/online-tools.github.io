fcf867947f92727457e512c6dfd227cd3c0d416e
mahovich <nigirma@yandex.ru>
2018-10-24 11:00:06 +0300

    Dependency update

---
+++ precache-manifest.12cf7dfd2ebea85cd421100d332e4126.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b1bed5b0db4bde6c7e14",
+    "url": "/static/js/main.b1bed5b0.chunk.js"
+  },
+  {
+    "revision": "282f1a5a37d0631467cf",
+    "url": "/static/js/1.282f1a5a.chunk.js"
+  },
+  {
+    "revision": "b1bed5b0db4bde6c7e14",
+    "url": "/static/css/main.810e986f.chunk.css"
+  },
+  {
+    "revision": "7157f9cef6fc88cae31cde6b9d1d30f6",
+    "url": "/index.html"
+  }
+];
