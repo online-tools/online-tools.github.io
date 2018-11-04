42f20368df8eb12904384eee9614d24245314786
mahovich <nigirma@yandex.ru>
2018-11-04 14:04:47 +0300

    Added options for opening multiple URLs: separator between lines; the number of opened URLs at a time

---
+++ precache-manifest.aaec4630e49c8d3238febb02c4b908bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fa3741220d93c5b9be55",
+    "url": "/static/js/main.fa374122.chunk.js"
+  },
+  {
+    "revision": "1c562974a7f92aa7cdc2",
+    "url": "/static/js/1.1c562974.chunk.js"
+  },
+  {
+    "revision": "fa3741220d93c5b9be55",
+    "url": "/static/css/main.a908ecf0.chunk.css"
+  },
+  {
+    "revision": "0f712795a1c07b7177527dee73dc76bb",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98997f17b367121aed7f0a4d11c94160.js"
+  "/precache-manifest.aaec4630e49c8d3238febb02c4b908bb.js"
 );
 
 workbox.clientsClaim();
